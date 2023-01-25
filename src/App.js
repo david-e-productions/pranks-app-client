e3230d714b609073e3d3f3445bd50b79a87931a1
David Amberg <david.amberg@ymail.com>
2023-01-25 17:40:18 +0100

    added branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in no time
         </a>
       </header>
     </div>
