33c67ba5fe92de6ddf96850de15743b14758bc30
David Amberg <david.amberg@ymail.com>
2023-02-01 21:41:46 +0100

    added hover for images on allpranks page

--- src/components/PrankCardList.jsx
+++ src/components/PrankCardList.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function PrankCardList(props) {
-  const { title, description, _id } = props;
+  const { title, description,imageUrl, _id } = props;
+  const {handleHover} = props
+
+  // console.log(imageUrl)
+ 
+
+  // const handleHover = (event) => {
+  //   setSelectedId(event.target.id);
+  //   console.log('seclected',event.target.id)
+  // };
+
   return (
-    <div className="PrankCard">
+    <div   className="PrankCard">
       <Link style={{ textDecoration: "none" }} to={`/pranks/${_id}`}>
-        <h1 style={{ fontSize: "40px" }}>{title}</h1>
+        <h1  onMouseEnter={handleHover} id={imageUrl}  style={{ fontSize: "40px" }}>{title}</h1>
       </Link>
-      <p className="prankCardDetailCard m-b-20">{description}</p>
+      <p  className="prankCardDetailCard m-b-20">{description}</p>
     </div>
   );
 }
